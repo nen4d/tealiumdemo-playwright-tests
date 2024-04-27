e87d9b56e01e4e09e1c8e7b37f798e9f597886ff
Nen4d <nen4d@protonmail.com>
2024-04-27 13:45:17 +0200

    Add test for account dashboard links

---
+++ tests/userAccountTests/accountDashboard.spec.ts
@@ -0,0 +1,55 @@
+import { test, expect } from '@playwright/test';
+import { loginPage } from '../../pages/loginPage.ts';
+import { accountPage } from '../../pages/accountPage.ts';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+dotenv.config({ path: path.resolve(__dirname, '..', 'my.env') });
+
+test('Account dashboard links', async ({page}) => {
+
+    const firstName = process.env.FIRST_NAME;
+    const lastName = process.env.LAST_NAME;
+    const emailAddress = process.env.EMAIL;
+    const password = process.env.PASSWORD;
+
+    const login = new loginPage(page);
+    await login.goToLoginPage();
+
+    const dashboardPage = new accountPage(page);
+
+    await login.loginIntoAccount(emailAddress, password);
+
+    await expect(page.getByRole('heading', { name: 'My Dashboard' })).toBeVisible();
+    await expect(page.getByText(`Hello, ${firstName} ${lastName}!`)).toBeVisible();
+    await expect(page).toHaveURL('/customer/account/');
+
+    const dashboardLinks = [
+        dashboardPage.editContactInfo,
+        dashboardPage.editNewsletter,
+        dashboardPage.editPassword,
+        dashboardPage.manageAddresses,
+        dashboardPage.editBillingAddress,
+        dashboardPage.editShippingAddress
+    ]
+
+    // Testing links on the dashboard page
+    const expectedPageURL = [
+        '/customer/account/edit/',
+        '/newsletter/manage/',
+        '/customer/account/edit/changepass/1/',
+        '/customer/address/new/',
+        '/customer/address/edit/',
+        '/customer/address/edit/'
+    ]
+
+    for(let i=0; i < dashboardLinks.length; i++) {
+        await dashboardPage.clickDashboardLinksAndAssert(
+            dashboardLinks[i],
+            expectedPageURL[i]
+        )
+    }
+
+})
