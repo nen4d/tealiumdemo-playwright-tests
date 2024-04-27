436b42e9a66169a7dd6a5cf6b0d88fd4bf17b39c
Nen4d <nen4d@protonmail.com>
2024-04-27 14:38:35 +0200

    Added test to edit account information, verify changes, and revert back to original data

--- pages/accountPage.ts
+++ pages/accountPage.ts
@@ -3,12 +3,25 @@ import Collection from '@lariat/playwright';
 
 export class accountPage extends Collection<Page> {
 
+    // Links from account dashboard
     readonly editContactInfo = this.page.getByRole('link', { name: 'Edit' }).first();
     readonly editNewsletter = this.page.getByRole('link', { name: 'Edit' }).nth(1);
     readonly editPassword = this.page.getByRole('link', { name: 'Change Password' });
     readonly manageAddresses = this.page.getByRole('link', { name: 'Manage Addresses' });
     readonly editBillingAddress = this.page.getByRole('link', { name: 'Edit Address' }).first();
     readonly editShippingAddress = this.page.getByRole('link', { name: 'Edit Address' }).first();
+
+    // Left navigation of account page
+    readonly accountInformationMenu = this.page.getByRole('link', { name: 'Account Information' });
+
+    // Inputs
+    readonly firstName = this.page.getByLabel('*First Name');
+    readonly lastName = this.page.getByLabel('*Last Name');
+    readonly currentPassword = this.page.getByLabel('*Current Password');
+
+    // Buttons
+    readonly saveChangesButton = this.page.getByRole('button', { name: 'Save' });
+
 
     async goToDashboardPage() {
         await this.page.goto('https://ecommerce.tealiumdemo.com/customer/account/');
@@ -24,6 +37,16 @@ export class accountPage extends Collection<Page> {
         // Navigate back to the home page
         await this.goToDashboardPage();
 
+    }
+
+    async editInformationsAndAssert(firstNameField, lastNameField, passwordField) {
+        await this.firstName.fill(firstNameField);
+        await this.lastName.fill(lastNameField);
+        await this.currentPassword.fill(passwordField);
+        await this.saveChangesButton.click();
+
+        await expect(this.page.getByText(`Hello, ${firstNameField} ${lastNameField}!`)).toBeVisible();
+        await expect(this.page.locator('p.hello')).toHaveText(`Hello, ${firstNameField} ${lastNameField}!`);
     }
 
 }
--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -17,4 +17,6 @@ export class loginPage extends Collection<Page> {
         await this.loginButton.click();
     }
 
+    
+
 }
